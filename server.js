4b36ae5265aacad8386ad28328213c30f71d4191
Jahangir Ripon <jripon1991@gmail.com>
2026-05-15 20:24:00 +0600

    remove keys

--- server.js
+++ server.js
@@ -2,21 +2,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable prettier/prettier */
 const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const mongoose = require('mongoose');
 const app = require('./app');
 
 console.log(process.env.DATABASE);
 
 mongoose
-  .connect(
-    'mongodb+srv://connectericj_db_user:pgA7RrL746IlzGt7@cluster0.larpuhs.mongodb.net/natours?appName=Cluster0',
-  )
-  .then((con) => {
-    // console.log(con.connections);
-    console.log('Connected!');
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.end' });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
