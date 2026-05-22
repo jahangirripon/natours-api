fb1c64ff02c413d589fc7038c9ac69bc9f099efb
Jahangir Ripon <jripon1991@gmail.com>
2026-05-22 15:59:44 +0600

    mongo aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -141,3 +141,43 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          avgRating: { $avg: '$ratingsAverage' },
+          numOfRatings: { $sum: '$ratingsQuantity' },
+          numOfTours: { $sum: 1 },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      data: {
+        status: 'success!',
+        stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      data: {
+        status: 'Fail!!!',
+        message: error,
+      },
+    });
+  }
+};
