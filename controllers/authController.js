8bc83f682422e5501ac050a92f6351e62d5fbdd4
Jahangir Ripon <jripon1991@gmail.com>
2026-06-16 20:42:06 +0600

    added mailtrap and send reset mail

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const { promisify } = require('util');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const Mail = require('../utils/mail');
+const sendEmail = require('../utils/mail');
 
 const signToken = (id) => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -88,13 +90,38 @@ exports.restrictTo = (...roles) => {
 };
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // http://127.0.0.1:3000/api/v1/users/resetPassword/26565
+
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
     return next(new AppError('No user found!', 401));
   }
-
-  const resetToken = user.createPasswordResetToken();
+  const resetToken = await user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
+  const resetURL = `${req.protocol}:${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot password? Submit PATCH with password and passwordConfirm to: ${resetURL}.`;
+
+  console.log(resetURL);
+  console.log(message);
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to mail!',
+    });
+  } catch (err) {
+    ((user.passwordResetToken = undefined),
+      (user.passwordResetExpires = undefined),
+      await user.save({ validateBeforeSave: false }));
+    return next(new AppError('Error!!!!!', 500));
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
