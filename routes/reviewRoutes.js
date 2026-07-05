862293c69dc21de399ba070d459da0afcc8847d7
Jahangir Ripon <jripon1991@gmail.com>
2026-07-05 20:05:11 +0600

    nested routes & create review mod with tour Id

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,12 +2,7 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
-
-router
-  .route('/')
-  .get(authController.protect, reviewController.getAllReviews)
-  .post(reviewController.createReview);
+const router = express.Router({ mergeParams: true });
 
 router.use((req, res, next) => {
   next();
@@ -17,13 +12,30 @@ router.use((req, res, next) => {
   next();
 });
 
+// // router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user', 'guide', 'admin', 'lead-guide'),
+//     reviewController.createReview,
+//   );
+
+router
+  .route('/')
+  .get(authController.protect, reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user', 'guide', 'admin', 'lead-guide'),
+    reviewController.createReview,
+  );
+
 router
   .route('/:id')
   .get(reviewController.getReview)
   .patch(reviewController.updateReview)
   .delete(
     authController.protect,
-    // authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo('user', 'guide', 'admin', 'lead-guide'),
     reviewController.deleteReview,
   );
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,8 +2,20 @@ const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
+// const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user', 'guide', 'admin', 'lead-guide'),
+//     reviewController.createReview,
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -32,14 +44,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user', 'guide', 'admin', 'lead-guide'),
-    reviewController.createReview,
   );
 
 module.exports = router;
